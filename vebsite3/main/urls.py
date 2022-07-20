1476cc9a8171c6cf5fa7b68a8b4a5abb34c9e4fe
baham <bahamut95@mail.ru>
2022-07-20 20:51:34 +0300

    start

--- vebsite3/main/urls.py
+++ vebsite3/main/urls.py
@@ -4,4 +4,5 @@ from .import views
 urlpatterns = [
     path('', views.index),
     path('reshka/', views.reshka),
+    path('about/', views.about),
 ]
--- vebsite3/main/views.py
+++ vebsite3/main/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(requst):
-    return HttpResponse("<h4>Hello</h4>")
+    return render(requst, 'index.html')
 
 def reshka(requst):
     return HttpResponse("<h4>Что сегодня?</h4>")
+
+def about(requst):
+    return HttpResponse("<h4>About</h4>")
